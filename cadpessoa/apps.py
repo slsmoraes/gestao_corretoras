3414e88ff10daaebabc8506b7104cb2041549cbc
slsmoraes <slsmoraes@gmail.com>
2021-04-06 12:10:57 -0300

    D20210406H1210-Criando a tabela Qualificação

--- cadpessoa/apps.py
+++ cadpessoa/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class CadPessoaConfig(AppConfig):
-    name = 'cad_pessoa'
+    name = 'cadpessoa'
---
+++ qualificacao/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from qualificacao.models import Qualificacao
+
+# Register your models here.
+admin.site.register(Qualificacao)
